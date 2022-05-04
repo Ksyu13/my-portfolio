2b1b65d71c28e1fa211e14e839174320eb9aae59
Ksyu13 <ksyu413@gmail.com>
2022-05-05 00:15:50 +0300

    Added block with my works

---
+++ src/works/Works.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './Works.module.css';
+import styleContainer from '../common/styles/Container.module.css';
+import Work from './work/Work';
+
+function Works() {
+    return (
+        <div className={style.worksBlock}>
+            <div className={`${styleContainer.conteyner} ${style.worksContainer}`}>
+                <h2 className={style.title}>My Works</h2>
+                <div>
+                    <p>Check out my latest web software development portfolio projects.</p>
+                </div>
+                <div className={style.works}>
+                    <Work title={"Сайт 1"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem facilis reiciendis dicta iusto explicabo perferendis reprehenderit aspernatur repellat blanditiis quis, quibusdam cupiditate accusamus architecto nam incidunt, repellendus magni! Ratione, itaque!Lorem ipsum dolor sit amet consectetur adipisicing elit."}/>
+                    <Work title={"Сайт 2"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}/>
+                    <Work title={"Сайт 3"} description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem facilis reiciendis dicta iusto explicabo perferendis reprehenderit aspernatur repellat blanditiis quis, quibusdam cupiditate accusamus architecto nam incidunt, repellendus magni! Ratione, itaque!"}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Works;
---
+++ src/works/work/Work.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Work.module.css';
+
+
+function Work(props) {
+    return (
+        <div className={style.work}>
+            <div className={style.image}>
+                <button>Whatch</button>
+            </div>
+                <h3>{props.title}</h3>
+                <span className={style.description}>
+                    {props.description}
+                </span>            
+        </div>
+
+    );
+}
+
+export default Work;
