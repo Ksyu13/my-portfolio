9555969e06315ae11a2086678bc62ff3de49313f
Ksyu13 <ksyu413@gmail.com>
2022-06-08 17:57:15 +0300

    nav all

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -7,17 +7,7 @@ function Nav() {
     <div className={style.nav}>
       <a href="#main">Main</a>
       <a href="#skills">Skills</a>
-      {/* <a href="#works">Projects</a> */}
-      {/* <Link
-        activeClass={style.active}
-        to="works"
-        spy={true}
-        smooth={true}
-        offset={50}
-        duration={500}
-        onSetActive={this.handleSetActive}
-      >Projects
-      </Link> */}
+      <a href="#works">Projects</a>
       <a href="#contacts">Contacts</a>
     </div>
   );
